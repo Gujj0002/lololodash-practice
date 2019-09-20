33801e368fb5432466216bbafeb34b639cdd5b38
Alpesh Gujjar <gujj0002@algonquinlive.com>
2019-09-20 16:49:28 -0400

    Count-Comments

---
+++ 6-Count-Comments.js
@@ -0,0 +1,25 @@
+const _ = require("lodash");
+
+const commentcount = function (comments) {
+
+    var counted = [];
+
+    // Group by username
+    comments = _.groupBy(comments, "username");
+
+    _.forEach(comments, function (item, name) {
+
+        counted.push({
+            username: name,
+            comment_count: _.size(item)
+        });
+    });
+
+    return _.sortBy(counted, "comment_count").reverse();
+    // alternative with a sorted function
+    // return _.sortBy(counted, function(comment) { return -comment.comment_count; });
+};
+
+
+
+module.exports = commentcount;
